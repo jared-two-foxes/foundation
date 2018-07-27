1c6d99d7526c0257742f2c901aac1e91894bd509
Jared Watt <Jared@twofoxes.co.nz>
2018-07-27 15:24:46 +1200

    REmove duplicate function

--- include/string_helper.hpp
+++ include/string_helper.hpp
@@ -1,11 +1,14 @@
 #ifndef FOUNDATION_STRING_HELPER_HPP__
 #define FOUNDATION_STRING_HELPER_HPP__
 
+#include <algorithm>
 #include <iomanip>
 #include <sstream>
 #include <string>
+#include <vector>
 
-namespace ui
+
+namespace foundation
 {
 
 
@@ -67,24 +70,6 @@ inline std::string repeat(unsigned n, std::string const& s) {
 	}
 	return result;
 }
-
-
-inline std::vector<std::string> split( std::string str, std::string token ) {
-    std::vector<std::string>result;
-    while(str.size()){
-        int index = str.find(token);
-        if(index!=std::string::npos){
-            result.push_back(str.substr(0,index));
-            str = str.substr(index+token.size());
-            if(str.size()==0)result.push_back(str);
-        }else{
-            result.push_back(str);
-            str = "";
-        }
-    }
-    return result;
-}
-
 
 }
 
